63db05cebe5c86090a9504fdb2b7f8abc173cdbb
CGM <mrmouse.guru@gmail.com>
2024-05-20 10:57:11 +0700

    added OO calculating a * b

---
+++ src/oop/Calculator.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Calculator {
+
+    //fields, attribute, instance
+    int a;
+    int b;
+
+    //method, function => behavior
+    Calculator(){
+        a = 2;
+        b = 2;
+
+    }
+
+    int multiply(){
+        return a * b;
+    }
+
+}
---
+++ src/oop/CalculatorApp.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+
+        Calculator calculatorRemote = new Calculator();
+        int result = calculatorRemote.multiply();
+        System.out.println("Ket qua : " + result );
+    }
+
+}
